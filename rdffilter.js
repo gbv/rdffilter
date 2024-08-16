4f5b23030817f34bc139c04644385856416a1c78
Jakob Voss <voss@gbv.de>
2024-08-16 21:47:15 +0200

    Move file

--- rdffilter.js
+++ rdffilter.js
@@ -1,5 +1,5 @@
 import N3 from "n3"
-import { Transform }  from "stream"
+import { RDFFilterTransformer } from "./src/transformer.js"
 
 function error(msg, code=1) {
   console.error(`${msg}`)
@@ -15,74 +15,6 @@ export const formats = {
   turtle: "text/turtle",
 }
 
-class RDFFilter extends Transform {
-  constructor(filter, options={}) {
-    super({
-      objectMode: true,
-      transform(quad, _, callback) {        
-        this.stats.quads++
-          
-        var result = filter(quad)
-        if (result === true) {
-          this.keep(quad)
-        } else if (Array.isArray(result)) {
-          if (!result.length) {
-            this.remove(quad)
-          } else {
-            if (quad.equals(result[0])) {
-              this.keep(quad)
-              result.shift()
-            }
-            for (let q of result) {
-              this.add(q)
-            }
-          }
-        } else if (typeof result === "object") {
-          if (quad.equals(result[0])) {
-            this.keep(quad)
-          } else {
-            this.remove(quad)
-            this.add(result)
-          }
-        } else {
-          this.remove(quad)
-        }
-        callback(null)
-      },
-    })
-
-    this.stats = { quads: 0, kept: 0, removed: 0, added: 0 }
-    this.pass = {
-      added: options.added ?? true,
-      kept: options.kept ?? true,
-      removed: options.removed ?? false,
-    }
-  }
-
-  keep(quad) {
-    if (this.pass.kept) {
-      this.push(quad)
-      this.stats.kept++
-    }
-  }
-
-  remove(quad) {
-    if (this.pass.removed) {
-      this.push(quad)
-    } else {
-      this.stats.removed++
-    }
-  }
-
-  add(quad) {
-    if (this.pass.added) {
-      this.push(quad)
-      this.stats.added++
-    }
-  }
-}    
-
-
 // TODO: try https://www.npmjs.com/package/async-transforms for parallel?
 
 export function rdffilter(input, output, options = {}) {
@@ -95,12 +27,12 @@ export function rdffilter(input, output, options = {}) {
   var format = from ? formats[from] : "turtle" 
   const parser = new N3.StreamParser({ format })
 
-  // TODO: prefixes for nice output?
+  // TODO: Add prefixes for nice output?
   // TODO: make sure output is valid, see https://github.com/rdfjs/N3.js/issues/383#issuecomment-2282261922
   format = to ? formats[to] : "N-Triples" 
   const writer = new N3.StreamWriter({ format })
 
-  filter = new RDFFilter(filter ?? (() => true), filterOptions)
+  filter = new RDFFilterTransformer(filter ?? (() => true), filterOptions)
 
   // TODO: pass errors to caller
   parser.on("error", error)
---
+++ src/transformer.js
@@ -0,0 +1,70 @@
+import { Transform }  from "stream"
+
+export class RDFFilterTransformer extends Transform {
+  constructor(filter, options={}) {
+    super({ objectMode: true })
+
+    this.filter = filter
+    this.stats = { quads: 0, kept: 0, removed: 0, added: 0 }
+    this.pass = {
+      added: options.added ?? true,
+      kept: options.kept ?? true,
+      removed: options.removed ?? false,
+    }
+  }
+
+  _transform(quad, _, callback) {        
+    this.stats.quads++
+          
+    var result = this.filter(quad)
+    if (result === true) {
+      this.keep(quad)
+    } else if (Array.isArray(result)) {
+      if (!result.length) {
+        this.remove(quad)
+      } else {
+        if (quad.equals(result[0])) {
+          this.keep(quad)
+          result.shift()
+        }
+        for (let q of result) {
+          this.add(q)
+        }
+      }
+    } else if (typeof result === "object") {
+      if (quad.equals(result[0])) {
+        this.keep(quad)
+      } else {
+        this.remove(quad)
+        this.add(result)
+      }
+    } else {
+      this.remove(quad)
+    }
+    callback(null)
+  }
+
+  keep(quad) {
+    if (this.pass.kept) {
+      this.push(quad)
+      this.stats.kept++
+    }
+  }
+
+  remove(quad) {
+    if (this.pass.removed) {
+      this.push(quad)
+    } else {
+      this.stats.removed++
+    }
+  }
+
+  add(quad) {
+    if (this.pass.added) {
+      this.stats.added++
+      this.push(quad)
+    }
+  }
+}    
+
+
